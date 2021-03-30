caea1f37b82b4ef885f05e08f34ba2ce71466821
HyunMok-Chung <1021hm@naver.com>
2021-03-31 00:21:42 +0900

    공동 레포지토리

--- riot.py
+++ riot.py
@@ -7,7 +7,7 @@ class RiotData:
             key=json.load(json_file)
         self.userID=userID
         self.api_key = key("Riot_Key")
-        self.url="https://kr.api.riotgames.com"
+        self.url="https://kr.api.riotgames.com" #공식 API
         self.DataDragon = "http://ddragon.leagueoflegends.com/cdn/11.6.1/data/ko_KR/" #게임 내 정적 컨텐츠
     def getUserData(self): #유저 정보
         url = self.url+"/lol/summoner/v4/summoners/by-name/"+self.userID+"?api_key="+self.api_key
