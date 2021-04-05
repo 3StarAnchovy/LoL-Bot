93e7e8d1b459e99abcbf2dd5bf8d6ffef86bca36
HyunMok-Chung <1021hm@naver.com>
2021-04-05 20:38:51 +0900

    티어, 승률 출력 완료//

--- riot.py
+++ riot.py
@@ -10,7 +10,7 @@ class RiotData:
     def __init__(self,userID): #set
         with open('key.json') as json_file:
             json_data = json.load(json_file)
-        self.userID=userID
+        self.userID = userID
         self.api_key = json_data['Riot_Key'] #로컬에 있는 json 파일의 value로 키 세팅
         self.url = "https://kr.api.riotgames.com"
         self.DataDragon = "http://ddragon.leagueoflegends.com/cdn/11.6.1/data/ko_KR"
@@ -18,12 +18,31 @@ class RiotData:
     def getUserData(self): #유저 정보
         url = self.url+"/lol/summoner/v4/summoners/by-name/"+self.userID+"?api_key="+self.api_key
         res = requests.get(url)
-        sum_Lev = res.json().get('summonerLevel')
-        sum_ID = res.json().get('id')
+        sum_Lev = res.json().get('summonerLevel') #유저 레벨
+        sum_ID = res.json().get('id')  #전적검색 위한 유저ID
         print("소환사 레벨 : ", sum_Lev)
         print("summonerID : ", sum_ID)
 
-    
+        url_League = self.url + "/lol/league/v4/entries/by-summoner/" + sum_ID + "?api_key=" + self.api_key
+        res_League = requests.get(url_League)
+        queue_List = res_League.json()  #소환사 티어, 승률, json파일 입력
+
+        que_Type = input("솔랭 or 자랭 : ")
+
+        if que_Type == "자랭":  #자랭 검색
+            print("티어 : ", queue_List[0].get('tier'), queue_List[0].get('rank'), queue_List[0].get('leaguePoints'), "포인트")
+            #티어, 랭크, 리그포인트 출력
+            win = int(queue_List[0].get('wins'))
+            lose = int(queue_List[0].get('losses'))
+            print("승률 : ", round((win/(lose+win))*100, 1), "%")  #승률 출력
+        elif que_Type == "솔랭":  #솔랭 검색
+            print("티어 : ", queue_List[1].get('tier'), queue_List[1].get('rank'), queue_List[1].get('leaguePoints'), "포인트")
+            #티어, 랭크, 리그포인트 출력
+            win = int(queue_List[1].get('wins'))
+            lose = int(queue_List[1].get('losses'))
+            print("승률 : ", round((win/(lose+win))*100, 1), "%")  #승률 출력
+
+
     def getChampRotation(self): #챔프 로테이션
         url = self.url + "/lol/platform/v3/champion-rotations"+"?api_key="+self.api_key
         res = requests.get(url)
@@ -46,10 +65,3 @@ test = RiotData(userID)
 test.getUserData()
 print("\n")
 test.getChampRotation()
-
-#test1
-
-#api_key = "RGAPI-e829d34c-823a-48e2-8496-f25301e2ceee"
-#url ="https://kr.api.riotgames.com"+"/lol/summoner/v4/summoners/by-name/"+"HongJiMin"+"?api_key="+api_key
-#res = requests.get(url)
-#브런치 힘들어,,
